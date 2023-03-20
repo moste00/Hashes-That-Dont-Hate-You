5198d45d71d9646ba681432f9bc7ed805348b2d1
moste00 <ubermenchun@gmail.com>
2023-03-20 23:38:42 +0200

    Implemented a much more efficient checks for used slots

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,23 +1,34 @@
+import java.util.Arrays;
 import java.util.BitSet;
 
 public class HashTable<K,V> {
 
+    //The index array consists of "Slots", each slot is 2 array positions, the first holds a hash and the second holds an offseted index into the KV store (index + 1)
+    //Slots always begin at even positions, so index[0] and index[1] is one slot, index[2] and index[3] is another, etc...
+    //A Slot can be "marked" a certain state by setting the second position to an invalid index value (0, -1, -2, etc...)
     private int[] index ;
-    private BitSet indexSlotFilled;
+    //A slot in the index can be in one of 3 states, depending on the value of its second field
+    //State 1 : Filled, the data in the hash field and the index field is valid, in this case the index field lies in 1..MAX_INT
+    //State 2 : Empty, the data in the hash field and the index field is NOT valid, and the slot was never filled with valid data
+    private static final int INDEX_SLOT_EMPTY = 0;
+    //State 3 : Deleted, the data in the hash field and the index field is NOT valid, but the slot used to be occupied with valid data before
+    private static final int INDEX_SLOT_DELETED = -1;
+
+    //A plain old growable array with both a capacity and a size, capacity is the .length field of the array
     private Object[] kvStore;
-    int kvSize;
+    private int kvSize;
+
 
     public static final int DEFAULT_INITIAL_CAPACITY = 8 ;
     public static final float THRESHOLD_LOAD_FACTOR_FOR_INDEX_REBUILD = 0.65f;
     public HashTable(int initialCapacity) {
         if (initialCapacity < DEFAULT_INITIAL_CAPACITY) initialCapacity = DEFAULT_INITIAL_CAPACITY ;
 
+        //All 0s be default, all slots marked empty for free
         this.index = new int[2*initialCapacity];
-        //The ith bit is a flag for whether the slot at {H: index[2*i],I: index[2*i+1]} is filled or not
-        this.indexSlotFilled = new BitSet(initialCapacity);
 
         this.kvStore = new Object[2*initialCapacity];
-        //number of entries in the kvStore, i.e. the length of the actually occupied space (i.e. all indices from 0 to kvSize excluding kvSize are valid data)
+        //The actually occupied space (i.e. all data at indices from 0 to kvSize excluding kvSize itself are valid data)
         //always increases by 2
         this.kvSize = 0;
     }
@@ -33,22 +44,30 @@ public class HashTable<K,V> {
     }
 
     public V get(K key) {
-        int hashPosInIndex = lookupIndex(key.hashCode());
-        if (hashPosInIndex == -1) return null;
+        int i = getKvPosForHash(key.hashCode());
+        //hash doesn't exist, so key definitely doesn't exist
+        if (i == -1) return null;
 
-        int i = index[hashPosInIndex+1];
+        //otherwise, there is **something** that hashes to key.hashcode() that exist in our KV store, but it's not necessarily the key since hashes can collide
         //noinspection unchecked
-        K k = (K) kvStore[i];   //no worries
+        K k = (K) kvStore[i];   //no worries about the cast
         //noinspection unchecked
-        V v = (V) kvStore[i+1]; //it's okay, we know for a fact that it's of type V, the set method is type-safe
+        V v = (V) kvStore[i+1]; //it's okay, we know for a fact that it's of type V, the set method is type-safe and nobody else can put in the KvStore
 
         //fast identity check, implies equality but avoids a potentially slow .equal() call
         if (k == key) return v;
-        //identity not equal, still possible for equality to hold
+        //identity is not equal, but it's still possible for equality to hold
         if (key.equals(k)) return v;
 
-        //The key is not actually in the KV store and the hash we found above was a collision
+        //The key is not actually in the KV store and the hash we found above was indeed a collision
         return null;
+    }
+
+    private int getKvPosForHash(int hash) {
+        int hashPosInIndex = lookupIndex(index,hash);
+        if (hashPosInIndex == -1) return -1;
+
+        return index[hashPosInIndex+1] - 1;
     }
 
     private void insertKvStore(Object k, Object v) {
@@ -68,49 +87,67 @@ public class HashTable<K,V> {
         float currentLoadFactor = ((float)kvSize)/index.length;
         if (currentLoadFactor > THRESHOLD_LOAD_FACTOR_FOR_INDEX_REBUILD) rebuildIndex();
 
-        insertIndex(index,indexSlotFilled,hash,i);
+        insertIndex(index,hash,i);
     }
     private void rebuildIndex() {
         int[] newIndex = new int[2*index.length];
-        BitSet newIndexSlotFilled = new BitSet(2*indexSlotFilled.size());
 
         for (int i = 0; i<index.length; i += 2) {
-            if (indexSlotFilled.get(i/2)) {
-                insertIndex(newIndex,newIndexSlotFilled,index[i],index[i+1]);
+            boolean slotEmptyOrDeleted = index[i+1] <= INDEX_SLOT_EMPTY;
+
+            if (!slotEmptyOrDeleted) { //i.e. it's filled with valid data
+                insertIndex(newIndex,index[i],index[i+1]-1);
             }
         }
-
         index = newIndex;
-        indexSlotFilled = newIndexSlotFilled;
     }
 
-    private static void insertIndex(int[] index, BitSet indexSlotFilled, int hash, int i) {
+    private static void insertIndex(int[] index, int hash, int i) {
         int insertionPos = Math.abs(hash % index.length);
         //make it even, because slots are always aligned on even indices
         insertionPos = insertionPos & 0xFF_FF_FF_FE; //ensure the last bit is 0, meaning an even number, and leave the rest unaffected
 
         //linear probing on finding out that the chosen slot is already taken
-        while (indexSlotFilled.get(insertionPos/2)) {
+        boolean slotEmptyOrDeleted = index[insertionPos+1] <= INDEX_SLOT_EMPTY;
+        while (!slotEmptyOrDeleted) {
             insertionPos = (insertionPos+2) % index.length;
+            slotEmptyOrDeleted = index[insertionPos+1] <= INDEX_SLOT_EMPTY;
         }
-        indexSlotFilled.set(insertionPos/2);
         index[insertionPos] = hash;
-        index[insertionPos+1] = i;
+        index[insertionPos+1] = i+1;
     }
 
-    private int lookupIndex(int hashCode) {
+    private static int lookupIndex(int[] index,int hashCode) {
         int pos = Math.abs(hashCode % index.length);
         pos = pos & 0xFF_FF_FF_FE;
 
-        while (indexSlotFilled.get(pos/2) && index[pos] != hashCode) {
-            pos = (pos+2) % index.length;
-        }
-        if (indexSlotFilled.get(pos/2)) {
-            return pos;
-        }
-        else {
-            return -1;
-        }
-    }
+        boolean slotEmpty = index[pos+1] == INDEX_SLOT_EMPTY;
+        boolean slotDeleted = index[pos+1] == INDEX_SLOT_DELETED;
+        int firstDeletedSlot = -1;
 
+        while (!slotEmpty) {
+            if (slotDeleted) {
+                if (firstDeletedSlot == -1) firstDeletedSlot = pos;
+            }
+            //slot is neither empty nor deleted, i.e. it's filled, i.e. let's see the hash there
+            else {
+                //found our guy
+                if (index[pos] == hashCode) break;
+            }
+
+            //If the slot was deleted or it has a different hash from the one we're looking for, keep going
+            pos = (pos+2) % index.length;
+            slotEmpty = index[pos+1] == INDEX_SLOT_EMPTY;
+            slotDeleted = index[pos+1] == INDEX_SLOT_DELETED;
+        }
+        if (slotEmpty) return -1;
+
+        //relocate the found element to the first deleted slot so that we find it faster the next time
+        if (firstDeletedSlot != -1) {
+            index[firstDeletedSlot] = hashCode;
+            index[firstDeletedSlot+1] = index[pos+1];
+            index[pos+1] = INDEX_SLOT_DELETED;
+        }
+        return pos;
+    }
 }
